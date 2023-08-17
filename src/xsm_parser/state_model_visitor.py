b8b35e3b9f02b9d167faa67a1043becc9f5abaeb
Leon Starr <leon_starr@modelint.com>
2023-08-17 16:18:41 -0700

    reversed grammar order and updated visitor for consistency but not tested

--- src/xsm_parser/state_model_visitor.py
+++ src/xsm_parser/state_model_visitor.py
@@ -19,62 +19,89 @@ class StateModelVisitor(PTNodeVisitor):
     """Visit parsed units of an Executable UML State Model"""
     # Each header comment below corresponds to section in state_model.peg file
 
-    # Elements
-    def visit_nl(self, node, children):
-        """New line character"""
-        return None
-
-    def visit_sp(self, node, children):
-        """Single space character"""
-        return None
-
-    def visit_name(self, node, children):
-        """Model element name"""
-        name = ''.join(children)
-        return name
-
-    def visit_body_line(self, node, children):
-        """Lines that we don't need to parse yet, but eventually will"""
-        # TODO: These should be attributes and actions
-        body_text_line = children[0]
-        return body_text_line
-
-    # State block
-    def visit_state_name(self, node, children):
-        """Model element name"""
-        name = ''.join(children)
-        return name
-
-    def visit_creation(self, node, children):
-        return 'creation'
-
-    def visit_deletion(self, node, children):
-        return 'deletion'
-
-    def visit_parameter_name(self, node, children):
-        """Model element name"""
-        name = ''.join(children)
-        return name
-
-    def visit_type_name(self, node, children):
-        """All characters composing a data type name"""
-        name = ''.join(children)
-        return name
-
-    def visit_parameter(self, node, children):
-        """param_name type_name"""
-        return Parameter_p(name=children[0], type=children[1])
-
-    def visit_parameter_set(self, node, children):
-        """list of { param_name: type_name } pairs"""
+    # Root
+    @classmethod
+    def visit_statemodel(cls, node, children):
+        """ EOL* metadata? domain_header (lifecycle / assigner) events* initial_transitions* state_block* EOF """
         return children
 
-    def visit_signature(self, node, children):
-        """Strips out parentheses"""
+    # Metadata
+    @classmethod
+    def visit_metadata(cls, node, children):
+        """ metadata_header data_item* """
+        items = {k: v for c in children for k, v in c.items()}
+        return items
+
+    @classmethod
+    def visit_data_item(cls, node, children):
+        """ INDENT name SP* (resource_item / text_item) EOL* """
+        return { children[0]: children[1] }
+
+    @classmethod
+    def visit_resource_item(cls, node, children):
+        """ '>' SP* name """
+        return ''.join(children), True  # Item, Is a resource
+
+    @classmethod
+    def visit_text_item(cls, node, children):
+        """ ':' SP* r'.*' """
+        return children[0], False  # Item, Not a resource
+
+    # Scope headers
+    @classmethod
+    def visit_domain_header(cls, node, children):
+        """ "domain" SP name EOL* """
+        name = children[0]
+        return name
+
+    @classmethod
+    def visit_lifecycle(cls, node, children):
+        """ "class" SP name EOL* """
+        return {'class': children[0] }
+
+    @classmethod
+    def visit_assigner(cls, node, children):
+        """ "relationship" SP rnum EOL* """
+        return {'rel': children[0] }
+
+    # Events
+    @classmethod
+    def visit_events(cls, node, children):
+        """A list of event names"""
+        return list(children)
+
+    @classmethod
+    def visit_event_spec(cls, node, children):
+        """ INDENT event_name EOL* """
         return children[0]
 
-    def visit_state_header(self, node, children):
+    @classmethod
+    def visit_event_name(cls, node, children):
+        """ name """
+        name = ''.join(children)
+        return name
+
+    # Initial transitions
+    @classmethod
+    def visit_initial_transitions(cls, node, children):
+        """ it_header transition* block_end """
+        return children
+
+    # State block
+    @classmethod
+    def visit_state_block(cls, node, children):
+        """ state_header activity transitions? block_end """
+        s = children[0]  # State info
+        a = children[1]  # Activity (could be empty, but always provided)
+        t = [] if len(children) < 3 else children[2]  # Optional transitions
+        sblock = StateBlock_p(state=s, activity=a, transitions=t)
+        return sblock
+
+    @classmethod
+    def visit_state_header(cls, node, children):
         """
+        "state" sp state_name signature? (sp deletion)? EOL*
+
         There are four possible cases:
             state name
             state name (deletion)
@@ -89,77 +116,91 @@ class StateModelVisitor(PTNodeVisitor):
             sig = children[1]
         return StateSpec_p(name=n, deletion=deletion, signature=sig)
 
-    def visit_transition(self, node, children):
-        """event destination_state"""
-        return Transition_p(event=children[0], to_state=None if len(children) < 2 else children[1])
+    @classmethod
+    def visit_signature(cls, node, children):
+        """ '()' / '(' SP? parameter_set SP? ')' """
+        return children[0]
 
-    def visit_transitions(self, node, children):
-        """All transitions exiting a state including any creation transitions"""
+    @classmethod
+    def visit_parameter_set(cls, node, children):
+        """ parameter (',' SP parameter)* """
         return children
 
-    def visit_activity(self, node, children):
-        """Required state activity, which may or may not contain any actions"""
-        return children
+    @classmethod
+    def visit_parameter(cls, node, children):
+        """ parameter_name SP? ':' SP? type_name """
+        return Parameter_p(name=children[0], type=children[1])
 
-    def visit_state_block(self, node, children):
-        """All state data"""
-        s = children[0]  # State info
-        a = children[1]  # Activity (could be empty, but always provided)
-        t = [] if len(children) < 3 else children[2]  # Optional transitions
-        sblock = StateBlock_p(state=s, activity=a, transitions=t)
-        return sblock
-
-    # Initial transitions
-    def visit_initial_transitions(self, node, children):
-        return children
-
-    # Events
-    def visit_event_name(self, node, children):
-        """Model element name"""
+    @classmethod
+    def visit_parameter_name(cls, node, children):
+        """ name """
         name = ''.join(children)
         return name
 
-    def visit_event_spec(self, node, children):
-        """event_name"""
-        return children[0]
-
-    def visit_events(self, node, children):
-        """A list of event names"""
-        return list(children)
-
-    # Scope
-    def visit_assigner(self, node, children):
-        """Scope: If value supplied this is an assigner state model"""
-        return {'rel': children[0] }
-
-    def visit_lifecycle(self, node, children):
-        """Scope: If value supplied this is a lifecycle state model"""
-        return {'class': children[0] }
-
-    def visit_domain_header(self, node, children):
-        """domain_name"""
-        name = children[0]
+    @classmethod
+    def visit_type_name(cls, node, children):
+        """ name """
+        name = ''.join(children)
         return name
 
-    # Metadata
-    def visit_text_item(self, node, children):
-        return children[0], False  # Item, Not a resource
+    @classmethod
+    def visit_DELETION(cls, node, children):
+        """ r'!*' """
+        return 'deletion'
 
-    def visit_resource_item(self, node, children):
-        return ''.join(children), True  # Item, Is a resource
-
-    def visit_item_name(self, node, children):
-        return ''.join(children)
-
-    def visit_data_item(self, node, children):
-        return { children[0]: children[1] }
-
-    def visit_metadata(self, node, children):
-        """Meta data section"""
-        items = {k: v for c in children for k, v in c.items()}
-        return items
-
-    # Root
-    def visit_statemodel(self, node, children):
-        """The complete state machine (state model)"""
+    @classmethod
+    def visit_activity(cls, node, children):
+        """ activity_header body_line* """
         return children
+
+    @classmethod
+    def visit_transitions(cls, node, children):
+        """ transition_header transition* """
+        return children
+
+    @classmethod
+    def visit_transition(cls, node, children):
+        """ INDENT event_name (SP '>' SP state_name)? EOL* """
+        return Transition_p(event=children[0], to_state=None if len(children) < 2 else children[1])
+
+    @classmethod
+    def visit_state_name(cls, node, children):
+        """ name """
+        name = ''.join(children)
+        return name
+
+    # Elements
+    @classmethod
+    def visit_body_line(cls, node, children):
+        """ INDENT r'.*\n' """
+        body_text_line = children[0]
+        return body_text_line
+
+    @classmethod
+    def visit_name(cls, node, children):
+        """ word (delim word)* """
+        name = ''.join(children)
+        return name
+
+    # Discarded whitespace and comments
+    @classmethod
+    def visit_LINEWRAP(cls, node, children):
+        """
+        EOL SP*
+        end of line followed by optional INDENT on next line
+        """
+        return None
+
+    @classmethod
+    def visit_EOL(cls, node, children):
+        """
+        SP* COMMENT? '\n'
+
+        end of line: Spaces, Comments, blank lines, whitespace we can omit from the parser result
+        """
+        return None
+
+    @classmethod
+    def visit_SP(cls, node, children):
+        """ ' '  Single space character (SP) """
+        return None
